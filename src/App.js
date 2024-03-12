d081845485b041711f5aeee1ad6e6fff2bcbf468
Rohit-Sahu-01 <rohitsahu98377@gmail.com>
2024-03-12 14:19:14 +0530

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
+import 'moment-timezone'
 import './App.css';
+import Part_One from './Part_One';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <Part_One/></>
+
   );
 }
 
